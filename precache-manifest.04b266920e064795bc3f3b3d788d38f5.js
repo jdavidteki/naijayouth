f95768db9efab49854b470f17016dfe226f6c650
= <jdavid@yext.com>
2020-11-09 01:52:10 -0500

    Updates

---
+++ precache-manifest.04b266920e064795bc3f3b3d788d38f5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/naijayouth/favicon-16.png"
+  },
+  {
+    "revision": "e84886a63ba63349c509",
+    "url": "/naijayouth/static/js/app.3ccf40d0.chunk.js"
+  },
+  {
+    "revision": "352af39fd46dbc3c3f8c",
+    "url": "/naijayouth/static/js/runtime~app.298b0a63.js"
+  },
+  {
+    "revision": "2903b48d2274bf69457f",
+    "url": "/naijayouth/static/js/2.33110511.chunk.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/naijayouth/serve.json"
+  },
+  {
+    "revision": "ea5befcaf810366fff855f028619851d",
+    "url": "/naijayouth/register-service-worker.js"
+  },
+  {
+    "revision": "fe6a830f9d0335063dc77df89e6d407d",
+    "url": "/naijayouth/manifest.json"
+  },
+  {
+    "revision": "b9eb64fc468b64e1c90f43449ff8e1c5",
+    "url": "/naijayouth/index.html"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/naijayouth/favicon.ico"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/naijayouth/favicon-32.png"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/naijayouth/expo-service-worker.js"
+  }
+];
